d6949da500d2498542f9adff336e36456f890caa
planetceles <mandecmande@gmail.com>
2026-03-13 14:14:33 -0600

    prophets

---
+++ scripts/prophets.js
@@ -0,0 +1,38 @@
+const url = "https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json";
+const cards = document.querySelector("#cards");
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    // console.table(data.prophets);
+    displayProphets(data.prophets);
+}
+getProphetData();
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        let card = document.createElement("section");
+        // card.classList.add("temple-card");
+        let fullName = document.createElement("h2");
+        let date_birth = document.createElement("p");
+        let place_birth = document.createElement("p");
+        let portrait = document.createElement("img");
+        card.classList.add("prophet-card");
+
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        date_birth.textContent = `Date of Birth: ${prophet.birthdate}`;
+        place_birth.textContent = `Place of Birth: ${prophet.birthplace}`;
+        portrait.setAttribute("src", prophet.imageurl);
+        portrait.setAttribute("alt", `Portrait of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute("loading", "lazy");
+        portrait.setAttribute("width", "340");
+        portrait.setAttribute("height", "440");
+
+        card.appendChild(fullName);
+        card.appendChild(date_birth);
+        card.appendChild(place_birth);
+        card.appendChild(portrait)
+
+        cards.appendChild(card);
+    })
+}
