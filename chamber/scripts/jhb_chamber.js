c61da7099f71c957065d9d6512f2573d95627518
planetceles <mandecmande@gmail.com>
2026-03-16 16:42:29 -0600

    member

--- chamber/scripts/jhb_chamber.js
+++ chamber/scripts/jhb_chamber.js
@@ -16,3 +16,42 @@ const lastModified = document.querySelector("#last-modified");
 if (lastModified) {
     lastModified.textContent = `Last Modified: ${document.lastModified}`;
 }
+
+// fetch data
+const url = "https://planetceles.github.io/wdd231/chamber/data/members.json";
+const cards = document.querySelector("#member");
+
+async function getMembersData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayMembers(data.members);
+}
+getMembersData();
+
+const displayMembers = (members) => {
+    members.forEach((member) => {
+        let card = document.createElement("section");
+        let fullName = document.createElement("h3");
+        let phone = document.createElement("p");
+        let website = document.createElement("p");
+        let portrait = document.createElement("img");
+        card.classList.add("member-card");
+
+        fullName.textContent = `${member.name} ${member.lastname}`;
+        phone.textContent = `Phone Number: ${member.phone}`;
+        website.textContent = `Website: ${member.website}`;
+        portrait.setAttribute("src", member.imageurl);
+        portrait.setAttribute("alt", `${member.name}`);
+        portrait.setAttribute("loading", "lazy");
+        portrait.setAttribute("width", "400");
+        portrait.setAttribute("height", "auto");
+
+        card.appendChild(portrait);
+        card.appendChild(fullName);
+        card.appendChild(phone);
+        card.appendChild(website);
+        
+
+        cards.appendChild(card);
+    });
+}
