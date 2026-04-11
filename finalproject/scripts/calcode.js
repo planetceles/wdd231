aee09d82bfb50912fac8ba5585731d1b1cff3ee3
planetceles <mandecmande@gmail.com>
2026-04-11 16:07:29 -0600

    icon

--- finalproject/scripts/calcode.js
+++ finalproject/scripts/calcode.js
@@ -2,6 +2,26 @@ import { features } from "../data/features.mjs";
 
 const featureSection = document.querySelector("#features-container");
 
-const displayFeathres = (features) => {
-    
+const displayFeatures = (features) => {
+    features.forEach((feature) => {
+        let card = document.createElement("section");
+        let title = document.createElement("h3");
+        let description = document.createElement("p");
+        let visual = document.createElement("p");
+        let iconCard = document.createElement("div");
+        card.classList.add("feature-card");
+        iconCard.classList.add("icon-card");
+
+        title.textContent = `${feature.title}`;
+        description.textContent = `${feature.description}`;
+        visual.textContent = `${feature.icon}`;
+
+        iconCard.appendChild(visual);
+        card.appendChild(iconCard);
+        card.appendChild(title);
+        card.appendChild(description);
+
+        featureSection.appendChild(card);
+    });
 }
+displayFeatures(features);
